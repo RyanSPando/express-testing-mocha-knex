e9bdf210d7b5b79d88e1c59a45c86f645a70a74b
Ryan "archaen" Pando <vretha@gmail.com>
2016-09-08 09:22:33 -0600

    Knex wired up.

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,12 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: 'postgresql://localhost/knex_kittens',
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: __dirname + '/src/server/db/migrations'
     },
-    pool: {
-      min: 2,
-      max: 10
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
-
 };
